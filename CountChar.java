2a35a89254f0fbe1fe4b5d1ff0d54f949322f679
Amarnath Sakthivel <63423850+Amarnath-Sakthivel@users.noreply.github.com>
2020-05-15 10:49:59 -0400

    Update CountChar.java

--- CountChar.java
+++ CountChar.java
@@ -11,41 +11,18 @@ public class CountChar {
         for(int i=0;i<arr.length;i++){
             if(arr[i]=='2' || arr[i]=='3' || arr[i]=='5' || arr[i]=='7') {
                     primeNumbers += arr[i];
+                    sum+=(Character.getNumericValues(arr[i]));
                 }
-            else if(arr[i]!='2' || arr[i]!='3' || arr[i]!='5' || arr[i]!='7'){
-                count++;
-            }
-
+                else{
+                    count++;
+                    }
         }
         if(count==arr.length) {
             System.out.println(arr[0]+":"+arr[(arr.length)-1]);  // print first : last character
         }
-
-
         else {
-            char[] array=primeNumbers.toCharArray();
-            int len=array.length;
-            for (int i = 0; i < len; i++)
-        {
-            for (int j = i+1; j < len; j++)
-            {
-                if(array[i] == array[j])
-                {
-                    array[j] = array[len-1];
-                    len--;
-                    j--;
-                }
-            }
-        }
-        char[] array1 = Arrays.copyOf(array, len);
-            for (int i = 0; i < array1.length; i++)
-        {
-            duplicate+=array1[i];
-            sum+=(Character.getNumericValue(array[i]));    // change character to numerical value and sum it.
-        }
-            System.out.println(duplicate+ ":" + String.valueOf(sum)); // print prime numbers : its sum
-
-        }
+            System.out.println(primeNumbers+":"+String.valueOf(sum));
+           }
         return null;
 
         }
